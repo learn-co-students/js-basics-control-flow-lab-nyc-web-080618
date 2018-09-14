81af9904c2f6f78f1d8a5e688a61cdb4f0251531
rendnil <andrew.lindner.15@gmail.com>
2018-09-14 18:57:59 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your code in this file!
+function scuberGreetingForFeet(someNumber){
+
+
+  if (someNumber <=400){
+    return "This one is on me!"
+
+  }else if(someNumber > 2000 && someNumber< 2500){
+    return "I will gladly take your thirty bucks."
+  }else if (someNumber > 2500){
+    return "No can do."
+  }
+}
+
+function ternaryCheckCity(destination){
+  return( destination == "NYC"? "Ok, sounds good.": "No go.")
+}
+
+function switchOnCharmFromTip(tip){
+  if(tip == "generous"){
+    return "Thank you so much."
+  }else if(tip == "not as generous"){
+    return "Thank you."
+
+  }else{
+    return "Bye."
+
+  }
+
+
+
+}
