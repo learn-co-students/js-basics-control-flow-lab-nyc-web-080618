33d900b75ea7afe98dd4eb5c8a17c240dbfec728
mccoleman75225 <michaelcoleman19@gmail.com>
2018-09-14 14:21:46 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your code in this file!
+function scuberGreetingForFeet(value){
+  if (value < 400) {
+    return "This one is on me!";
+  }
+  else if (value > 2000 && value < 2500){
+    return "I will gladly take your thirty bucks.";
+  }
+  else {
+    return "No can do.";
+  }
+}
+
+
+function ternaryCheckCity(entered_city){
+  if (entered_city === "NYC"){
+    return "Ok, sounds good.";
+  }
+  else {
+    return "No go."
+  }
+}
+
+function switchOnCharmFromTip(entered_tip){
+  if (entered_tip === "generous"){
+    return "Thank you so much.";
+  }
+  else if (entered_tip === 'not as generous'){
+    return "Thank you."
+  }
+  else {
+    return "Bye."
+  }
+
+}
