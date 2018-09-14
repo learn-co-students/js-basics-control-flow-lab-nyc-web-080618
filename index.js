610852a16c0fd804ebf441c41018493bd8f2e1e2
github username <ariellenramirez@gmail.com>
2018-09-14 16:03:52 -0400

    Done.

--- index.js
+++ index.js
@@ -6,27 +6,20 @@ function scuberGreetingForFeet(feet){
   }else if(2000 <= feet && feet <= 2500){
     return 'I will gladly take your thirty bucks.';
   }else{
-    return 'No can do.'
+    return 'No can do.';
   }
-
 }
-
-
 function ternaryCheckCity(city){
-  if(city === 'NYC'){
-    return 'Ok, sounds good.'
-  }else{
-    return 'No go.'
-  }
+  return city === 'NYC'? 'Ok, sounds good.' : 'No go.';
 }
-
 
 function switchOnCharmFromTip(tip){
-   if(tip === 'generous'){
-     return 'Thank you so much.'
-   }else if(tip === 'not as generous'){
-     return 'Thank you.'
-   }else{
-    return 'Bye.'
-  }
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+    }
 }
