989f73a212e800fed01ca3726bead5f6fa5289e9
Justin Wang <justinw0827@gmail.com>
2018-09-15 17:16:32 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your code in this file!
+function scuberGreetingForFeet(distance) {
+  let response = "";
+  if (distance <= 400) {
+    response = "This one is on me!";
+  } else if (distance > 2000 && distance <= 2500) {
+    response = "I will gladly take your thirty bucks.";
+  } else if (distance > 2500) {
+    response = "No can do.";
+  } else {
+
+  }
+
+  return response;
+}
+
+function ternaryCheckCity(city) {
+  return city === "NYC" ? "Ok, sounds good." : "No go."
+}
+
+function switchOnCharmFromTip(tip) {
+  let response = "";
+  switch (tip) {
+    case "generous":
+      response = "Thank you so much."
+      break;
+    case "not as generous":
+      response = "Thank you."
+      break;
+    default:
+      response = "Bye."
+  }
+
+  return response;
+}
